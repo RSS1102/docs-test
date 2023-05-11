599e4a27d4688070f83ccf4f7b0f55dbee1d0c42
Jimmy <jimmyrss1102@gmail.com>
2023-05-12 01:11:18 +0800

    fix ：path

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -16,7 +16,10 @@ export default defineConfig({
   markdown:{
     lineNumbers:true,
   },
+
   base: '/docs-test/',
+
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: '/electron-vite.svg',
@@ -40,48 +43,48 @@ export default defineConfig({
         text: 'Guide',
         collapsed: false,
         items: [
-          { text: 'Why Electron Vite', link: '/Guide/why-electron-vite' },
-          { text: 'Getting Started', link: '/Guide/getting-started' },
+          { text: 'Why Electron Vite', link: '/guide/why-electron-vite' },
+          { text: 'Getting Started', link: '/guide/getting-started' },
         ]
       },
       {
         text: 'Plugin',
         collapsed: false,
         items: [
-          { text: 'Vite-plugin-electron', link: '/Plugin/vite-plugin-electron' },
-          { text: 'Vite-plugin-electron-renderer', link: '/Plugin/vite-plugin-electron-renderer' },
+          { text: 'Vite Plugin Electron', link: '/plugin/vite-plugin-electron' },
+          { text: 'Vite Plugin Electron-renderer', link: '/plugin/vite-plugin-electron-renderer' },
         ]
       },
       {
         text: 'Build',
         collapsed: false,
         items: [
-          { text: 'Electron-builder', link: '/Build/Electron-builder' },
-          { text: 'PackagingStaticResource', link: '/Build/PackagingStaticResource' },
+          { text: 'Electron Builder', link: '/build/electron-builder' },
+          { text: 'Packaging Static Resource', link: '/build/packaging-static-resource' },
         ]
       },
       {
         text: 'Electron',
         collapsed: false,
         items: [
-          { text: 'Render Process Use Nodejs', link: '/Electron/RenderProcessUseNodejs' },
-          { text: 'Multiple Programs Windows', link: '/Electron/MultipleProgramsWindows' },
+          { text: 'Render Process Use Nodejs', link: '/electron/render-process-use-nodejs' },
+          { text: 'Multiple Programs Windows', link: '/electron/multiple-programs-windows' },
         ]
       },
       {
         text: 'Troubleshooting',
         collapsed: false,
         items: [
-          { text: 'Issue', link: '/Troubleshooting/Issue' },
+          { text: 'Issue', link: '/troubleshooting/issue' },
         ]
       },
       {
         text: 'Community',
         collapsed: false,
         items: [
-          { text: 'Team', link: '/Community/team' },
-          { text: 'Development And Contribution', link: '/Community/development-contribution' },
-          { text: 'Acknowledge', link: '/Community/acknowledge' },
+          { text: 'Team', link: '/community/team' },
+          { text: 'Development And Contribution', link: '/community/development-contribution' },
+          { text: 'Acknowledge', link: '/community/acknowledge' },
         ]
       }
     ],
@@ -89,5 +92,5 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/electron-vite' }
     ]
-  },
+  }
 })
